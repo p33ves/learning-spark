7dbb2137127c6ea674a57c89f7fa969aacaaff3a
Vignesh P <vignesh.p1110@gmail.com>
2020-07-22 23:28:02 +0530

    pyspark word_count

--- word_count.py
+++ word_count.py
@@ -1,8 +1,11 @@
 from pyspark import SparkContext
+from operator import add
 
 sc = SparkContext(appName="TestApp")
 desc_text = sc.textFile('descriptions.txt')
 words = desc_text.flatMap(lambda line: line.split(' '))
-word_count = words.map(lambda x: (x, 1))
-word_count.reduceByKey(lambda x, y: x+y).collect()
-print(vars(word_count))
+word_count = words.map(lambda word: (word, 1)).reduceByKey(add)
+output = word_count.collect()
+with open('output.txt', 'w') as f:
+    for word, count in output:
+        f.write(f"{word} : {count}\n")
