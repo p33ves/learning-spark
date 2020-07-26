1c6644d76bb4b25021c89ac46ed0c331c38407b5
Vignesh P <vignesh.p1110@gmail.com>
2020-07-26 23:42:04 +0530

    created word cleaner to remove .,"' chars

--- word_count.py
+++ word_count.py
@@ -1,6 +1,15 @@
 import pyspark
 from operator import add
 
+
+def word_cleaner(word: str) -> str:
+    new_word = word
+    for char in (',', '.', ':', '"', "'"):
+        if char in new_word:
+            new_word = new_word.replace(char, '')
+    return new_word
+
+
 spark = pyspark.sql.SparkSession \
     .builder \
     .appName("TestApp") \
@@ -9,8 +18,10 @@ spark = pyspark.sql.SparkSession \
 netflix_data = spark.read.csv('netflix_data.csv', header=True).rdd
 desc = netflix_data.map(lambda x: x[5])
 words = desc.flatMap(lambda line: str(line).split(' '))
-word_count = words.map(lambda word: (word, 1)).reduceByKey(add)
+word_count = words.map(lambda word: (word_cleaner(word), 1)).reduceByKey(add)
+# word[:-1], 1) if word[-1] in (',', '.', ':') and len(word[:-1]) else (word, 1))
+#word_count = words.map(lambda word: (word, 1)).reduceByKey(add)
 output = word_count.collect()
-with open('output.txt', 'w') as f:
+with open('output.txt', 'w', encoding="utf-8") as f:
     for word, count in output:
         f.write(f"{word} : {count}\n")
