77c04ce3252a467bd6a860f74d803c8d3892d951
Vignesh P <vignesh.p1110@gmail.com>
2020-07-28 21:53:10 +0530

    shortened intermediate steps

--- word_count.py
+++ word_count.py
@@ -20,8 +20,6 @@ netflix_data = spark.read.csv('netflix_data.csv', header=True).rdd
 desc = netflix_data.map(lambda x: x[5])
 words = desc.flatMap(lambda line: str(line).split(' '))
 word_count = words.map(lambda word: (word_cleaner(word), 1)).reduceByKey(add)
-# word[:-1], 1) if word[-1] in (',', '.', ':') and len(word[:-1]) else (word, 1))
-#word_count = words.map(lambda word: (word, 1)).reduceByKey(add)
 output = word_count.collect()
 with open('output.txt', 'w', encoding="utf-8") as f:
     for word, count in output:
